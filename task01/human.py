37fbc356de37f9a0a00577b30e226aa0f5697bb4
= <=>
2023-05-06 12:27:26 +0200

    Change Human class without Encapsulation

--- task01/human.py
+++ task01/human.py
@@ -1,37 +1,25 @@
 # Human - Entity Class
 class Human:
+
     def __init__(self, name='no name', age='1', alive=True):
-        self.__name = name
-        self.__age = age
-        self.__alive = alive
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, name):
-        if isinstance(name, str):
-            self.__name = name
-
-    @property
-    def age(self):
-        return self.__age
-
-    @age.setter
-    def age(self, age=1):
-        if 0 < age <= 120:
-            self.__age = age
-
-    @property
-    def is_alive(self):
-        return "Yes" if self.__alive else "No"
-
-    @is_alive.setter
-    def is_alive(self, alive):
-        if isinstance(alive, bool):
-            self.__alive = alive
+        self.name = name
+        self.age = age
+        self.alive = alive
 
     def __str__(self):
-        return f"{self.__name}: age = {self.__age}. " \
-               f"Is alive? - {self.is_alive}"
+        return (f"{self.name}: age = {self.age}. "
+                f"Is alive? - {'YES' if self.alive else 'NO'}")
+
+
+def main():
+    h1 = Human()
+    h1.name = 1234
+    h1.age = -20
+    print(h1)
+
+    h2 = Human("Kate", 18, False)
+    print(h2)
+
+
+if __name__ == "__main__":
+    main()
