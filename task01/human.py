11e2be0978bdb2625a43898c437e8c00ab1dec1d
= <=>
2023-05-06 12:30:57 +0200

    Change Human class with Encapsulation based on public get/set-methods

--- task01/human.py
+++ task01/human.py
@@ -2,19 +2,40 @@
 class Human:
 
     def __init__(self, name='no name', age='1', alive=True):
-        self.name = name
-        self.age = age
-        self.alive = alive
+        self.__name = name
+        self.__age = age
+        self.__alive = alive
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        if isinstance(name, str):
+            self.__name = name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        if isinstance(age, int) and 0 < age <= 120:
+            self.__age = age
+
+    def is_alive(self):
+        return self.__alive
+
+    def set_alive(self, alive):
+        if isinstance(alive, bool):
+            self.__alive = alive
 
     def __str__(self):
-        return (f"{self.name}: age = {self.age}. "
-                f"Is alive? - {'YES' if self.alive else 'NO'}")
+        return f"{self.__name}: age = {self.__age}. " \
+               f"Is alive? - {'YES' if self.__alive else 'NO'}"
 
 
 def main():
     h1 = Human()
-    h1.name = 1234
-    h1.age = -20
+    h1.set_name(1234)
+    h1.set_age(-20)
     print(h1)
 
     h2 = Human("Kate", 18, False)
